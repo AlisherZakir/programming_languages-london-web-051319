5b482df8d2e06e121c6d7a544b7589e18893bba2
github username <github email address>
2019-04-27 14:36:19 +0100

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,12 @@
 def reformat_languages(languages)
   # your code here
+  hash = {}
+  languages.each do |style, langs|
+      hash = hash.merge(langs) {|key, v1, v2| v1}
+      langs.keys.each do |language|
+        hash[language][:style] = [] if hash[language][:style] == nil
+        hash[language][:style] << style
+      end
+  end
+  hash
 end
